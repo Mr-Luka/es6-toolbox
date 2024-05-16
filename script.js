b383e1f925962c302c382cd25a109abdb771b2b5
Luka <luka.luka.luka93@gmail.com>
2024-05-16 13:22:23 -0700

    es6

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ function es5Function(name) {
     return "Hello, " + name + " from the functions of JavaScript past."
 }
 console.log(es5Function("Luka"));
+
+const es6Function = name => `Hello, from the future ${name}.`;
+console.log(es6Function("Luka"))
